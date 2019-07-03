04a82614007f43f2b993ec5c3654324c08e77450
Aranad <aran@organicdesign.co.nz>
2019-07-03 13:33:45 -0300

    need i18n now

---
+++ ExtraMagic.i18n.magic.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Internationalization file for the magic words
+ */
+$magicWords = [];
+
+$magicWords['en'] = [
+	'MAG_CURRENTUSER' => [0, 'CURRENTUSER'],
+	'MAG_CURRENTPERSON' => [0, 'CURRENTPERSON'],
+	'MAG_CURRENTLANG' => [0, 'CURRENTLANG'],
+	'MAG_CURRENTSKIN' => [0, 'CURRENTSKIN'],
+	'MAG_ARTICLEID' => [0, 'ARTICLEID'],
+	'MAG_IPADDRESS' => [0, 'IPADDRESS'],
+	'MAG_DOMAIN' => [0, 'DOMAIN'],
+	'MAG_GUID' => [0, 'GUID'],
+	'MAG_USERPAGESELFEDITS' => [0, 'USERPAGESELFEDITS'],
+	'REQUEST' => [0, 'REQUEST'],
+	'COOKIE' => [0, 'COOKIE'],
+	'USERID' => [0, 'USERID'],
+	'IFGROUP' => [0, 'IFGROUP'],
+	'IFUSES' => [0, 'IFUSES'],
+	'IFCAT' => [0, 'IFCAT'],
+	'PREV' => [0, 'PREV'],
+	'NEXT' => [0, 'NEXT'],
+	'OWNER' => [0, 'OWNER'],
+	'PRIVATE' => [0, 'PRIVATE']
+];
